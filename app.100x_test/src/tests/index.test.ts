c35658b1549f8fed286b8fe361eb8d43b14d7d8b
Krish Soni <sonikrish994@gmail.com>
2024-06-09 14:01:43 +0530

    end-to-end testing

---
+++ app.100x_test/src/tests/index.test.ts
@@ -0,0 +1,22 @@
+import {  } from 'cypress';
+
+describe('Testing app', () => {
+    beforeEach(() => {
+      cy.visit('https://app.100xdevs.com')
+    })
+  
+    it('is able to log in', () => {
+      cy.contains('Login').should('exist')
+      cy.contains('Login').click()
+      cy.contains('Signin to your Account').should('exist', { timeout: 10000 })
+      cy.get('#email').type('harkirat.iitr@gmail.com');
+  
+      cy.get('#password').type('123random');
+  
+      cy.get('button').eq(4).click()
+  
+      cy.contains('View Content').should("exist", {timeout: 10000})
+    })
+  
+  })
+  
